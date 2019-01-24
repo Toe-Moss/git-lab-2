d69c47af1ac54d3d3b4d10a252aeaa5ca36e8cc5
Tomas Sanchez <ts701717@ohio.edu>
2019-01-23 19:10:27 -0500

    git lab program

---
+++ git-lab-program.cc
@@ -0,0 +1,92 @@
+/* Tomas Sanchez
+   CS 2400 
+   January 23 2019
+------------------
+Answer 1: git version 2.17.1
+
+Answer 2: user.name=Tomas Sanchez
+     	  user.email=ts701717@ohio.edu
+
+Answer 3: prints a list in terminal of possible inputs following "git --" and their functions.
+
+Answer 4: On branch master
+	
+	  No commits yet
+	
+	  Untracked files:
+	
+	  (use "git add <file>..." to include in what will be committed)
+
+		README.md
+	     	answers.md
+
+	  nothing added to commit but untracked files present (use "git add" to track)
+
+Answer 5: On branch master
+
+	  No commits yet
+
+	  Changes to be committed:
+  	    (use "git rm --cached <file>..." to unstage)
+
+	          new file:   README.md
+
+	  Untracked files:
+  	    (use "git add <file>..." to include in what will be committed)
+
+		  answers.md
+
+Answer 6: On branch master
+
+	  No commits yet
+
+	  Changes to be committed:
+  	    (use "git rm --cached <file>..." to unstage)
+
+	          new file:   README.md
+		      new file:   answers.md
+Answer 7: On branch master
+	  nothing to commit, working tree clean
+
+Answer 8: commit 18ff1d1c8c4dc2a30d58919b6ecf2412ca209529 (HEAD -> master)
+	  Author: Tomas Sanchez <ts701717@ohio.edu>
+	  Date:   Wed Jan 23 18:31:24 2019 -0500
+
+    	      Initial commit
+
+Answer 9: On branch master 
+	  Your branch is up to date with 'origin/master'.
+
+	  nothing to commit, working tree clean
+
+Answer 10: The changes were not reflected: 
+	   Tomas Sanchez 
+	   Toe-Moss
+
+Answer 11: The command does not go through since the repository online has work that I do not have locally.
+
+Answer 12: # Tomas Sanchez 
+	   Toe-Moss
+	   ## CS 2400, Section 108
+
+Answer 13: "ls -a" lists all visible and hiddin files in current dirctory: 
+           .  ..  .git  .gitignore  README.md
+*/
+ /*
+ *        File: git-lab-program.cc
+ *      Author: Tomas Sanchez
+ *        Date: January 23, 2019
+ * Description: prints out "Hello Git!"
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+cout << "Hello Git!" << endl;
+    return 0;
+}            
